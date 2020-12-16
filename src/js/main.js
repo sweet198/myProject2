a79f8f5e134fcb32614601f52ccc1383701732f0
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-12-16 21:55:27 +0300

    configured mask for tel. inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import modals from "./modules/modals";
 import forms from "./modules/forms";
 import sliders from "./modules/sliders";
+import mask from "./modules/mask";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     forms();
     sliders('.feedback-slider','.feedback-slider-item', '', 5000, '.main-prev-btn', '.main-next-btn' );
     sliders('.main-slider','.main-slider-item', 'vertical', 5000);
+    mask('[name="phone"]');
 });
