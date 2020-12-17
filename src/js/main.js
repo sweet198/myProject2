6cc889d2dac279d12b621889608eefbc661583fb
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-12-17 08:35:49 +0300

    configured inputs checking for language

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import modals from "./modules/modals";
 import forms from "./modules/forms";
 import sliders from "./modules/sliders";
 import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -9,4 +10,6 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider','.feedback-slider-item', '', 5000, '.main-prev-btn', '.main-next-btn' );
     sliders('.main-slider','.main-slider-item', 'vertical', 5000);
     mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
