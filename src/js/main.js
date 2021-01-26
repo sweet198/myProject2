468af58a0d30a62a0be780cab140cb16ce584a67
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2021-01-26 23:43:49 +0300

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import sizes from "./modules/sizes";
 import accordion from "./modules/accordion";
 import burger from "./modules/burger";
 import scrolling from "./modules/scrolling";
+import drop from "./modules/drop";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
@@ -28,4 +29,5 @@ window.addEventListener('DOMContentLoaded', () => {
     accordion('.accordion-heading', '.accordion-block');
     burger('.burger-menu', '.burger');
     scrolling('.pageup');
+    drop();
 });
